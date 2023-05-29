4d76e734f555feb6e57ee01354d65f51ff014381
Python Blue <chrisbday@me.com>
2023-05-29 00:32:12 -0400

    Update package-osx-bundles.sh

--- scripts/package-osx-bundles.sh
+++ scripts/package-osx-bundles.sh
@@ -15,26 +15,26 @@ rm -rf lv2
 rm -rf vst3
 
 mkdir au lv2 vst3
-mv sfizz.component au/
-mv sfizz.lv2 lv2/
-mv sfizz.vst3 vst3/
+mv build/sfizz.component au/
+mv build/sfizz.lv2 lv2/
+mv build/sfizz.vst3 vst3/
 
 pkgbuild \
   --identifier "sfz.tools.sfizz.au.bundle" \
   --install-location "/Library/Audio/Plug-Ins/Components/" \
-  --root "${PWD}/build/au/" \
+  --root "${PWD}/au/" \
   sfz-tools-sfizz-au-bundle.pkg
 
 pkgbuild \
   --identifier "sfz.tools.sfizz.lv2.bundle" \
   --install-location "/Library/Audio/Plug-Ins/LV2/" \
-  --root "${PWD}/build/lv2/" \
+  --root "${PWD}/lv2/" \
   sfz-tools-sfizz-lv2-bundle.pkg
 
 pkgbuild \
   --identifier "sfz.tools.sfizz.vst3.bundle" \
   --install-location "/Library/Audio/Plug-Ins/VST3/" \
-  --root "${PWD}/build/vst3/" \
+  --root "${PWD}/vst3/" \
   sfz-tools-sfizz-vst3-bundle.pkg
 
 cd ..
